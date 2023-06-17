25e6b91a51a396cffebbaefaf1263c0f5057f1e1
Ivan-Malakhovskyi <124193519+Ivan-Malakhovskyi@users.noreply.github.com>
2023-06-17 22:32:11 +0300

    completed 9,10 tasts

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,18 @@
+const selectors = {
+  color: document.querySelector(".color"),
+  btn: document.querySelector(".change-color"),
+  body: document.querySelector("body"),
+};
+
+selectors.btn.addEventListener("click", onChangeBackgroundColor);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+function onChangeBackgroundColor() {
+  selectors.body.style.backgroundColor = getRandomHexColor();
+  selectors.color.textContent = getRandomHexColor();
 }
--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,34 @@
+const refs = {
+  div: document.querySelector("#controls"),
+  input: document.querySelector("input"),
+  buttonCreate: document.querySelector("[data-create]"),
+  buttonDestroy: document.querySelector("[data-destroy]"),
+  divBox: document.querySelector("#boxes"),
+};
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+refs.buttonCreate.addEventListener("click", createBoxes);
+refs.buttonDestroy.addEventListener("click", destroyBoxes);
+
+function createBoxes(amount) {
+  amount = Number(refs.input.value);
+  const boxes = [];
+  for (let i = 0; i < amount; i += 1) {
+    const boxSize = 30 + i * 10;
+    const box = document.createElement("div");
+    box.style.width = `${boxSize}px`;
+    box.style.height = `${boxSize}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxes.push(box);
+  }
+  refs.divBox.append(...boxes);
+}
+
+function destroyBoxes() {
+  refs.divBox.innerHTML = "";
 }
